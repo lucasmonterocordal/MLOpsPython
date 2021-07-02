d05be8826562471b3c7596809ea095ebf44a3114
lucas <lucas.montero.cordal@alumno.upm.es>
2021-07-02 10:47:06 +0200

    including double spacing between functions

--- nemesis/training/train.py
+++ nemesis/training/train.py
@@ -23,6 +23,8 @@ IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
 ARISING IN ANY WAY OUT OF THE USE OF THE SOFTWARE CODE, EVEN IF ADVISED OF THE
 POSSIBILITY OF SUCH DAMAGE.
 """
+
+
 import os
 import pandas as pd
 from sklearn.svm import SVC
@@ -33,12 +35,12 @@ from sklearn.model_selection import train_test_split
 def split_data(df):
     X = df.drop('Y', axis=1).values
     y = df['Y'].values
-
     X_train, X_test, y_train, y_test = train_test_split(
         X, y, test_size=0.2, random_state=0)
     data = {"train": {"X": X_train, "y": y_train},
             "test": {"X": X_test, "y": y_test}}
     return data
+
 
 # Train the model, return the model
 def train_model(data):
@@ -48,12 +50,14 @@ def train_model(data):
     reg_model.fit(data["train"]["X"], data["train"]["y"])
     return reg_model
 
+
 # Evaluate the metrics for the model
 def get_model_metrics(model, data):
     preds = model.predict(data["test"]["X"])
     accuracy = accuracy_score(preds, data["test"]["y"])
     metrics = {"accuracy": accuracy}
     return metrics
+
 
 def main():
     print("Running train.py")
@@ -69,6 +73,7 @@ def main():
     metrics = get_model_metrics(model, data)
     for (k, v) in metrics.items():
         print(f"{k}: {v}")
+
 
 if __name__ == '__main__':
     main()

2ed51d1de43b6d39eaf76bde1fa534cd77744014
lucas <lucas.montero.cordal@alumno.upm.es>
2021-07-06 11:35:36 +0200

    Suffle  values when splitting

--- nemesis/training/train.py
+++ nemesis/training/train.py
@@ -36,11 +36,34 @@ from sklearn.model_selection import train_test_split
 def split_data(df):
     X = df.drop('Y', axis=1).values
     y = df['Y'].values
-    X_train, X_test, y_train, y_test = train_test_split(
-        X, y, test_size=0.2, random_state=0)
+    # Lists for proper splitting
+    arrays_label = []
+    X_train_lst = []
+    X_test_lst = []
+    y_train_lst = []
+    y_test_lst = []    
+    for label in np.unique(y):  
+        index = np.where((y==label)) #return the index with that label
+        # Index has two arrays, one wiht the positions and one filled with zeros
+        # We add the positions to x and y
+        x_l = X[index[0],:] 
+        Y_l = y[index[0]]
+        #Split the values 70/30 in a random mode for the values acquired in the previous step
+        X_train_one_label, X_test_one_label, y_train_one_label, y_test_one_label = train_test_split( x_l, Y_l, train_size=0.7, test_size=0.3, shuffle=False)
+        #Complete list with all the labels 70/30
+        X_train_lst.append( X_train_one_label)
+        X_test_lst.append( X_test_one_label)
+        y_train_lst.append( y_train_one_label)
+        y_test_lst.append( y_test_one_label)
+    # Put in an array the lists for different classes
+    X_train = np.vstack(X_train_lst)
+    X_test = np.vstack(X_test_lst)
+    y_train = np.vstack(y_train_lst).flatten()    
+    y_test = np.vstack(y_test_lst)
     data = {"train": {"X": X_train, "y": y_train},
             "test": {"X": X_test, "y": y_test}}
     return data
+
 
 
 # Train the model, return the model
